64d117f42a90e8dfb698e7c777c8ca2493f319f9
Juan <navaju01@luther.edu>
2016-04-25 22:57:05 -0500

    resume

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('#myNavmenu a').on('click', function(){
+        if($('.navbar-toggle').css('display') !='none') {
+        $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
